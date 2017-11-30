31616de27849a74ede2fa439df3725fc40858bd8
Quernest <a.zhit4enko@gmail.com>
2017-11-30 09:45:26 +0300

    fix

--- app/services/API.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://raw.githubusercontent.com/Quernest/Schedule/master/API/it-14-1.json';
-
-class API {
-  static getData() {
-    return fetch(url);
-  }
-};
-
-export default API;
