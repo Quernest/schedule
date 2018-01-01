d772ac61573fa9d861b21d1d52f91489beb4b1eb
Quernest <a.zhit4enko@gmail.com>
2018-01-02 00:45:28 +0300

    Delete Api.js

--- app/services/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const url = 'https://schedule-admin.herokuapp.com/api';
-
-export default class API {
-  static get(type, ...params) {
-    return axios.get(`${url}/${type}/${params}`);
-  }
-}
