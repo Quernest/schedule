43a66e421f1c44a971147e7fc6e7fe253240876b
Quernest <a.zhit4enko@gmail.com>
2018-04-19 16:30:03 +0300

    home screen updated

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,12 +8,10 @@ import {
 import moment from 'moment';
 import type Moment from 'moment';
 
-import store from 'react-native-simple-store';
 import Loading from '../components/Loading';
 import Schedule from '../components/Schedule';
 import Calendar from '../components/Calendar';
-// import { isSameSemester } from '../helpers/helpers';
-// import { filterEvents } from '../helpers/filters';
+
 import API from '../services/api.service';
 
 export type EventType = {
@@ -66,23 +64,40 @@ type State = {
     name: string,
   },
   semesters?: Array<Object>,
+  events?: Array<EventType>,
   isLoading: boolean,
 };
 
-const filterEvents = (date: Moment, semesters: Array<Object>): Array<EventType> => {
+/**
+ * TODO:
+ * 
+ * - create external file for filterEvents
+ * - create external file for types
+ */
 
+const filterEvents = (date: Moment, semesters?: Array<Object>): Array<EventType> => {
   let currentSemester: SemesterType;
+
+  const dateFormat: string = 'DD/MM/YYYY';
+  const dateFormatWithoutYear: string = 'DD/MM/____';
 
   // detect current semester
   if (semesters && semesters.length) {
     semesters.map((semester) => {
       const { start, end } = semester;
 
-      const startDate = moment(start);
-      const endDate = moment(end);
+      const startDate = moment(new Date(start)).format(dateFormat);
+      const endDate = moment(new Date(end)).format(dateFormat);
+
+      const isMonthBetween = date.isBetween(
+        moment(startDate, dateFormatWithoutYear),
+        moment(endDate, dateFormatWithoutYear),
+        'month',
+        '[]',
+      );
 
       // using month() for missing year
-      if (startDate.month() <= date.month() && date.month() <= endDate.month()) {
+      if (isMonthBetween) {
         currentSemester = semester;
       }
 
@@ -90,12 +105,40 @@ const filterEvents = (date: Moment, semesters: Array<Object>): Array<EventType> 
     });
   }
 
-  const { schedule } = currentSemester;
+  // get filtered schedule
+  const {
+    schedule,
+    end,
+    start,
+    firstWeekType,
+  } = currentSemester;
 
-  // filter schedule of current semester
+  const startDate = moment(new Date(start)).format(dateFormat);
+  const endDate = moment(new Date(end)).format(dateFormat);
+
+  const weeksTotal = moment(endDate, dateFormatWithoutYear).isoWeek() - moment(startDate, dateFormatWithoutYear).isoWeek();
+  const weeksAfterStart = date.isoWeek() - moment(startDate, dateFormatWithoutYear).isoWeek();
+  const weeksBeforeEnd = moment(endDate, dateFormatWithoutYear).isoWeek() - date.isoWeek();
+
+  // console.log(weeksAfterStart, weeksBeforeEnd, weeksTotal);
+
+  let currentWeekType: number;
+
+  const oddWeek: number = 1;
+  const evenWeek: number = 2;
+
+  if (weeksAfterStart % 2 !== 0) {
+    currentWeekType = firstWeekType === oddWeek ? oddWeek : evenWeek;
+  } else {
+    currentWeekType = firstWeekType === evenWeek ? oddWeek : evenWeek;
+  }
+
   if (schedule && schedule.length) {
     return schedule.filter((event) => {
-      if (event.weekDay === moment(date).isoWeekday()) {
+      const isSameWeekType = event.weekType === currentWeekType;
+      const isSameWeekDay = event.weekDay === date.isoWeekday();
+
+      if (isSameWeekType && isSameWeekDay) {
         return event;
       }
     });
@@ -113,29 +156,36 @@ export default class HomeScreen extends React.Component<Props, State> {
   state = {
     currentDate: moment(),
     isLoading: true,
+    events: [],
   }
 
   componentDidMount() {
-    const tmpId = 71;
+    const id: number = 71;
 
-    this.getGroupAllData(tmpId);
+    this.getGroupAllData(id);
   }
 
   onDatePress = (date: Moment): void => {
     const { semesters } = this.state;
 
-    console.log(filterEvents(date, semesters));
-
     this.setState({
+      events: filterEvents(date, semesters),
       currentDate: date,
     });
   }
 
   getGroupAllData = async (id: number) => {
+    const { currentDate } = this.state;
+
     try {
       const data: dataType = await API.getGroupAllData(id);
+      const { group, semesters } = data;
 
-      this.setState({ ...data });
+      this.setState({
+        group,
+        semesters,
+        events: filterEvents(currentDate, semesters),
+      });
     } catch (error) {
       console.error(error);
     } finally {
@@ -149,6 +199,7 @@ export default class HomeScreen extends React.Component<Props, State> {
     const {
       currentDate,
       isLoading,
+      events,
     } = this.state;
 
     if (!isLoading) {
@@ -158,10 +209,10 @@ export default class HomeScreen extends React.Component<Props, State> {
             selectedDate={currentDate}
             onDatePress={this.onDatePress}
           />
-          {/* <Schedule
-            events={schedule}
+          <Schedule
+            events={events}
             date={currentDate}
-          /> */}
+          />
         </View>
       );
     }
